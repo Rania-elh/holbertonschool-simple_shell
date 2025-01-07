45f1cf0ddb4f0a2310e35709b7564d9b7dccb411
rania-elh <rania1elhefnawy@gmail.com>
2025-01-07 13:39:03 +0000

    Update the repo

---
+++ shellsimple1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a greeting message to the user
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "Hello, Holberton!";
+	printf("%s\n", message);
+	return (0);
+}
