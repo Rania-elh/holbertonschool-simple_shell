ff5e92444712536430ab992796948c0a3ef25690
rania-elh <rania1elhefnawy@gmail.com>
2025-01-08 17:26:58 +0100

    new update

---
+++ print_env.c
@@ -0,0 +1,20 @@
+#include "main.h"  /* Inclut le fichier principal contenant des fonctions et des déclarations nécessaires */
+
+/**
+ * print_env - Affiche les variables d'environnement.
+ *
+ * Cette fonction imprime les variables d'environnement sur la sortie standard.
+ * Chaque variable d'environnement est affichée sur une nouvelle ligne.
+ *
+ * Return: Pas de valeur retournée, c'est une fonction de type void.
+ */
+void print_env(void)
+{
+	char **env;  /* Déclare un pointeur de type tableau de chaînes de caractères pour parcourir les variables d'environnement */
+
+	/* Parcourt toutes les variables d'environnement jusqu'à ce que le pointeur arrive à NULL */
+	for (env = environ; *env != NULL; env++)  /* 'environ' est un tableau contenant les variables d'environnement */
+	{
+		printf("%s\n", *env);  /* Affiche chaque variable d'environnement suivie d'un saut de ligne */
+	}
+}
